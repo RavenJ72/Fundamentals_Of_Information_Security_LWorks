45bc319c64127e9d400558db94ca90fe37ea946a
RavenJ72 <gleb.nickolaenko@yandex.ru>
2022-09-12 10:50:13 +0300

    MatrixOfAlf

---
+++ src/LWork3.java
@@ -0,0 +1,49 @@
+public class LWork3 {
+    public static void main(String[] args) {
+
+        String[] alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя,№$)('>< ".split("");
+        String[][][] key  =  {
+                {
+                        "ьаебх)лгпш№нй>дюмкщъ,'эы$зоучвтфёия(<црсж|".split(""),
+                        "мжаэ'лк>я,двю|нчьецёзбъыт)щ<ги№пшоусй$(фрх".split(""),
+                        "lbъiбeatоmuqf)ns№wитcгkdшzhyoj<ыrущgп|зvpx".split(""),
+                        "d№tsmпjaq)ыiгтъ<шозr exиczlщkbofvуwhnyбupg".split(""),
+                },
+                {
+                        "э<л|созйкшгь№$ч')рунжвцпы(фдаеиёяхмщъ>юб,т".split(""),
+                        "ёбзчалцтх<пюдн№)м(сфв$щшьиъэе|гжоуйр',>кыя".split(""),
+                        "и<)jgзbpvhбпг|№fтnexmiщkzwodlоsшctrъыuyaуq".split(""),
+                        "qtkpxъоjшщh<smvптbegir№ozl)гзиydaы nufwбуc".split(""),
+                },
+                {
+                        "ь$я|'уыащър>и(гсдцлнчжфхмё,о)юйкбштвэзп№е<".split(""),
+                        "hxщaw)stjr|vоiпzldзыgъбcуnитшpufo№<mгeqbky".split(""),
+                        "гxоъmseи hбraщvlpw<тofш|зut№jdcqykzыупnig)".split(""),
+                        "щbsпptuvdxkуeошзl№тjhig<mыayzcq гrfбnoи)ъw".split(""),
+                },
+                {
+                        "оиск№$бэ'взпаждъч<ырй(ш,ньщфёт)|хе>юуялмгц".split(""),
+                        "гх№ц'чзслешкпыь,юрщ$|ёу>адф<вижъбм)(оэтйня".split(""),
+                        "|mrщгscywу tzbxhag)oneбpтшыivъ<udjfпlзиоq№".split(""),
+                        "№h)oipveпwщгтtylъgоjn<u kaбbmqrзzxcdиуsыfш".split(""),
+                }
+        };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
